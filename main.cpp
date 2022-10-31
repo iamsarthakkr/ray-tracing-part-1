637b0fa211c640ab217fb2fbd912af649405a935
sarthak kumar <sarthakkumar10151@gmail.com>
2022-11-01 02:36:39 +0530

    Rendering cyan to white gradient

--- main.cpp
+++ main.cpp
@@ -2,12 +2,36 @@
 
 #include "Vec3D.hpp"
 #include "Color.hpp"
+#include "Ray.hpp"
+
+// gives the color of a ray based on its y coordinate value
+// gradient varies from white for y = 0 to cyan (24, 163, 126)
+color ray_color(const Ray& ray) {
+   auto unit_direction = unit_vector(ray.direction());
+   auto t = 0.5 * (unit_direction.y() + 1.0);            // since y can be between [-1, 1], scaling it to be btw [0, 1]
+      
+   // linear interpolation : coloval = (1 - y) * initial_value + y * final_value
+   return (1 - t) * color(1.0, 1.0, 1.0) + t * color(24 / 255.999, 163 / 255.999, 126 / 255.999);
+}
 
 int main() {
    // Image dimensions
 
-   const int image_width = 256;
-   const int image_height = 256;
+   const auto aspect_ratio = 16.0 / 9.0;
+   const int image_width = 480;
+   const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+   // Setting up camera position
+   const auto origin = Vec3D(0, 0, 0);
+
+   // Setting virtual viewport dimension (to pass our rays through)
+   const auto viewport_height = 2.0;
+   const auto viewport_width = viewport_height * aspect_ratio;
+   const auto focal_length = 1.0;
+
+   const auto horizontal = Vec3D(viewport_width, 0, 0);                                                  // vector for width
+   const auto vertical = Vec3D(0, viewport_height, 0);                                                   // vector for height
+   const auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - Vec3D(0, 0, focal_length);    // vector to the lower left corner of the viewport
 
    // Render
 
@@ -18,11 +42,13 @@ int main() {
    for(int i = image_height - 1; i >= 0; --i) {
       std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
       for(int j = 0; j < image_width; j++) {
-         double r = (double) i / (image_height - 1);
-         double g = (double) j / (image_width - 1);
-         double b = 0.15;
+         auto y_offset = (double) i / (image_height - 1);
+         auto x_offset = (double) j / (image_width - 1);
 
-         Color::write_color(std::cout, color(r, g, b));
+         auto ray_direction = lower_left_corner + x_offset * horizontal + y_offset * vertical;
+         Ray ray(origin, ray_direction);
+         auto pixel_color = ray_color(ray);
+         Color::write_color(std::cout, pixel_color);
       }
    }
    std::cerr << "\nDone.\n";

c570a892a636349f268a1869b012ba2d776d2dbb
sarthak kumar <sarthakkumar10151@gmail.com>
2022-11-01 01:49:02 +0530

    Added Ray class

---
+++ include/Ray.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Vec3D.hpp"
+
+class Ray {
+public:
+   Ray() {}
+   Ray(const point3D& origin, const Vec3D& direction) : m_origin(origin), m_direction(direction) {}
+
+   point3D origin();
+   Vec3D direction();
+
+   point3D at(double t) const;
+   
+public:
+   point3D m_origin;
+   Vec3D m_direction;
+};

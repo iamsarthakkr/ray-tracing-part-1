e7e3c677a6e6d0ca96d1bd4ac56d31734f7af751
sarthak kumar <sarthakkumar10151@gmail.com>
2022-11-01 02:35:31 +0530

    Fixed Vec3D alias type decleration 2

--- include/Vec3D.hpp
+++ include/Vec3D.hpp
@@ -28,16 +28,49 @@ private:
 
 // Utility functions
 
-inline std::ostream& operator<<(std::ostream& out, const Vec3D& v);
-inline Vec3D operator+(const Vec3D& a, const Vec3D& b);
-inline Vec3D operator-(const Vec3D& a, const Vec3D& b);
-inline Vec3D operator*(const Vec3D& a, const Vec3D& b);
-inline Vec3D operator*(double t, const Vec3D& a);
-inline Vec3D operator*(const Vec3D& a, double t);
-inline Vec3D operator/(const Vec3D& a, double t);
-inline double dot(const Vec3D&a, const Vec3D& b);
-inline Vec3D cross(const Vec3D& a, const Vec3D& b);
-inline Vec3D unit_vector(const Vec3D& v);
+inline std::ostream& operator<<(std::ostream& out, const Vec3D& v) {
+   return out << v[0] << " " << v[1] << " " << v[2];
+}
+
+inline Vec3D operator+(const Vec3D& a, const Vec3D& b) {
+   return Vec3D(a[0] + b[0], a[1] + b[1], a[2] + b[2]);
+}
+
+inline Vec3D operator-(const Vec3D& a, const Vec3D& b) {
+   return Vec3D(a[0] - b[0], a[1] - b[1], a[2] - b[2]);
+}
+
+inline Vec3D operator*(const Vec3D& a, const Vec3D& b) {
+   return Vec3D(a[0] * b[0], a[1] * b[1], a[2] * b[2]);
+}
+
+inline Vec3D operator*(double t, const Vec3D& a) {
+   return Vec3D(a[0] * t, a[1] * t, a[2] * t);
+}
+
+inline Vec3D operator*(const Vec3D& a, double t) {
+   return t * a;
+}
+
+inline Vec3D operator/(const Vec3D& a, double t) {
+   return (1 / t) * a;
+}
+
+inline double dot(const Vec3D&a, const Vec3D& b) {
+   return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
+}
+
+inline Vec3D cross(const Vec3D& a, const Vec3D& b) {
+   return Vec3D(
+      a[1] * b[2] - a[3] * b[2],
+      -a[0] * b[2] + a[2] - b[0],
+      a[0] * b[1] - a[1] - b[0]
+   );
+}
+
+inline Vec3D unit_vector(const Vec3D& v) {
+   return v / v.length();
+}
 
 using point3D = Vec3D;  // 3D point
 using color = Vec3D;    // RGB color

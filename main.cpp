f22a03092547e05c19830bbe1201703a8d0befde
sarthak kumar <sarthakkumar10151@gmail.com>
2022-10-31 23:04:46 +0530

    Added simple rendered

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main() {
+   // Image dimensions
+
+   const int image_width = 256;
+   const int image_height = 256;
+
+   // Render
+
+   std::cout << "P3\n";
+   std::cout << image_width << " " << image_height << "\n";
+   std::cout << "255\n";
+
+   for(int i = image_height - 1; i >= 0; --i) {
+      for(int j = 0; j < image_width; j++) {
+         double r = (double) i / (image_height - 1);
+         double g = (double) j / (image_width - 1);
+         double b = 0.15;
+
+         int r_val = static_cast<int>(r * 255.999);
+         int g_val = static_cast<int>(g * 255.999);
+         int b_val = static_cast<int>(b * 255.999);
+
+         std::cout << r_val << " " << g_val << " " << b_val << '\n';
+      }
+   }
+}

5665dd6129d6fee0fec3a54d062b569f248ecc61
sarthak kumar <sarthakkumar10151@gmail.com>
2022-11-01 23:09:19 +0530

    Mofied params in class decleration

--- include/Diffused_Material.hpp
+++ include/Diffused_Material.hpp
@@ -9,7 +9,7 @@ public:
    Diffused_Material() : reflectance({1, 1, 1}) {}
    Diffused_Material(const color& reflectance) : reflectance(reflectance) {}
 
-   virtual bool scatter(const Ray& ray_in, const Hit_record& rec, color& attenuation, Ray& scatter) const override;
+   virtual bool scatter(const Ray& ray_in, const Hit_record& rec, color& attenuation, Ray& scattered) const override;
 
 private: 
    color reflectance;
--- include/Material.hpp
+++ include/Material.hpp
@@ -7,5 +7,5 @@
 class Material {
 public:   
    // method to check whether material scatters the incomming ray and also calculate by how much
-   virtual bool scatter(const Ray& ray_in, const Hit_record& rec, color& attenuation, Ray& scatter) const = 0;
+   virtual bool scatter(const Ray& ray_in, const Hit_record& rec, color& attenuation, Ray& scattered) const = 0;
 };
